2375f1478dd7207921f3648488bd876ca2486b70
dan <chronos937@gmail.com>
2019-10-26 13:05:46 -0400

    put functions inside a document.ready testing bandsintown response object

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,29 +1,32 @@
+$(document).ready(function(){
 
-$("#Button").click(function(){
-
+$("#Button").click(function () {
+    
+    var artist = $("#userInput").val().trim();
     var ytQueryURL = "https://www.googleapis.com/youtube/v3/playlists" + artist + "api=AIzaSyBcKMzNwbAtyNu07WyX9lJSU1VMgIueq0M"
-    var artist;
 
     $.ajax({
         url: ytQueryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
+        console.log(response.image_url);
     })
 
 
 
 
 });
-$("#search").click(function(){
+$("#search").click(function () {
 
-var bitQueryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=test";
-var artist;
+    var artist = $("#userInput").val().trim();
+    var bitQueryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=test";
 
-$.ajax({
-    url: bitQueryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
+    $.ajax({
+        url: bitQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
 })
 })
