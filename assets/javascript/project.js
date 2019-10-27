4134a7a821160c584b27f0ff2e5e0a9c93f4e74f
dan <chronos937@gmail.com>
2019-10-27 19:10:16 -0400

    made second ajax call write event info to the page with minimal styling and added class for css to make the results look better

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -40,6 +40,12 @@ $(document).ready(function () {
             method: "GET",
         }).then(function (response2) {
             console.log(response2);
+            for (i=0; i<response2.length; i++) {
+                var a = $("<div>");
+                a.addClass("tourInfo");
+                a.html(response2[i].datetime + " " + response2[i].venue.city + ", " + response2[i].venue.country + "<br>" + response2[i].venue.name);
+                $("#tourDates-div").append(a);
+            }
         })
     })
 })
