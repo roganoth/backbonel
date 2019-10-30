b50f5a0b7d830638be3135f391c8c37c268d7c52
Vy Balanzat <vybalanzat@gmail.com>
2019-10-29 20:01:36 -0400

    last fm similar artist top 5

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,29 +1,42 @@
 
-$("#Button").click(function(){
+$("#search").click(function () {
 
-    var ytQueryURL = "https://www.googleapis.com/youtube/v3/playlists" + artist + "api=AIzaSyBcKMzNwbAtyNu07WyX9lJSU1VMgIueq0M"
-    var artist;
+    var artist = $("#userInput").val().trim();
 
     $.ajax({
-        url: ytQueryURL,
-        method: "GET"
-    }).then(function(response){
+        url: "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=7f191238f1b38fc7abb83fcb39e8b91c&format=json",
+        method: "GET",
+
+
+    }).then(function (response) {
         console.log(response);
+
+        $("#tourDates-div").empty();
+
+
+        for (i = 0; i < response.similarartists.artist.length; i++) {
+            if (i < 5)  {
+            
+           var b = $("<p>").text(response.similarartists.artist[i].name);
+          $("#tourDates-div").append(b);
+            }
+       }
+
+
     })
 
 
-
-
 });
-$("#search").click(function(){
 
-var bitQueryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=test";
-var artist;
+//$("#search").click(function(){
 
-$.ajax({
-    url: bitQueryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-})
-})
+//var bitQueryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=test";
+//var artist = $("#userInput").val().trim();
+
+//$.ajax({
+//    url: bitQueryURL,
+ //   method: "GET"
+//}).then(function(response){
+  //  console.log(response);
+//})
+//})
