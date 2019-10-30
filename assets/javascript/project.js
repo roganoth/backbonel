73ee2e151420619822867851c2d6a057ed9d9020
Vy Balanzat <vybalanzat@gmail.com>
2019-10-29 21:10:49 -0400

    links for top5

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,36 +1,4 @@
 
-
-$("#search").click(function () {
-
-    var artist = $("#userInput").val().trim();
-
-    $.ajax({
-        url: "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=7f191238f1b38fc7abb83fcb39e8b91c&format=json",
-        method: "GET",
-
-
-    }).then(function (response) {
-        console.log(response);
-
-        $("#similarArtist").empty();
-
-
-        for (i = 0; i < response.similarartists.artist.length; i++) {
-            if (i < 5)  {
-            
-           var b = $("<p>").text(response.similarartists.artist[i].name);
-          $("#similarArtist").append(b);
-            }
-       }
-
-
-    })
-
-
-});
-
-
-
 $(document).ready(function () {
 
     $("#Button").click(function () {
@@ -92,4 +60,37 @@ $(document).ready(function () {
         })
     })
 })
+
+
+$("#search").click(function () {
+
+    var artist = $("#userInput").val().trim();
+
+    $.ajax({
+        url: "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=7f191238f1b38fc7abb83fcb39e8b91c&format=json",
+        method: "GET",
+
+
+    }).then(function (response) {
+        console.log(response);
+
+        $("#tourDates-div").empty();
+
+
+        for (i = 0; i < response.similarartists.artist.length; i++) {
+            if (i < 5)  {
+            
+           var b = $("<a>")
+           b.html(response.similarartists.artist[i].name + "<br>");
+           b.attr("href", response.similarartists.artist[i].url);
+          $("#tourDates-div").append(b);
+            }
+       }
+
+
+    })
+
+
+});
+
 
